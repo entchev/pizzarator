ea3d2287360d81c8012adf2e18ad007ea40471b9
entchev <micishi@gmail.com>
2020-12-24 20:04:01 +0000

    v5

--- backend/controllers/reviewController.js
+++ backend/controllers/reviewController.js
@@ -6,7 +6,7 @@ import Review from '../models/reviewModel.js'
 // @access Public
 
 const getReviews = asyncHandler(async (req, res) => {
-  const pageSize = 8
+  const pageSize = 16
   const page = Number(req.query.pageNumber) || 1
 
   const keyword = req.query.keyword
